390449669ff13b722014a1155f3b033e1c029ad5
beni <mamont4444@live.ru>
2026-01-31 21:42:29 +0200

    change

--- scenes/title.gd
+++ scenes/title.gd
@@ -1,10 +1,7 @@
 extends Control
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("confirm"):
-		call_deferred("change_scene")	
-	
-	
-	
-func change_scene():
+func _process(_delta: float) -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")	
+	
+	
+	
