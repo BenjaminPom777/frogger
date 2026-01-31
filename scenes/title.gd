f437250e05ee7652751fc7aa72f417d6b7a6410b
beni <mamont4444@live.ru>
2026-01-31 21:47:12 +0200

    fix confirm

--- scenes/title.gd
+++ scenes/title.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _process(_delta: float) -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")	
+	if Input.is_action_just_pressed("confirm"):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")	
 	
 	
 	
